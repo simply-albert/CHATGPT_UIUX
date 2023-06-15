45bc98442d6bff2044f3ed273039676ef358c1d7
simply-albert <albertochiengkiki@gmail.com>
2023-06-15 23:07:38 +0300

    Chatgpt setup

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './possibilty.css'
+
+const Possibilty = () => {
+    return(
+        <div>
+            Possibilty
+        </div>
+    )
+}
+
+export default Possibilty;
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '';l
+
+const WhatGPT3 = () => {
+  import '';lreturn 
+  (
+    <div>WhatGPT3</div>
+  )
+}
+import '';l
+
+export default WhatGPT3
